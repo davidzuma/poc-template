515c837e5cd46c073eea32c00ea56bb9235950e8
davidzuma <zumaquerodavid@gmail.com>
2024-06-30 16:01:45 +0200

    add logo to the app and new colors

--- {{cookiecutter.directory_name}}/src/{{cookiecutter.main_file}}.py
+++ {{cookiecutter.directory_name}}/src/{{cookiecutter.main_file}}.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="{{cookiecutter.project_name}}", page_icon=":material/airline_seat_flat_angled:")
 
+LOGO = "/Users/davidzumaquero/Github/davidzuma/poc-template/{{cookiecutter.directory_name}}/data/berlin_chess_bike.png"
+st.set_page_config(page_title="{{cookiecutter.project_name}}", page_icon=":material/airline_seat_flat_angled:")
+st.logo(LOGO)
 page_1= st.Page("pages/page_1.py", title="Page 1", icon=":material/add_circle:")
 page_2 = st.Page("pages/page_2.py", title="Page 2", icon=":material/airline_seat_flat_angled:")
 
