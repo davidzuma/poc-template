b416cf0da061b702b7566df2cf3faf7e7e13dd00
davidzuma <zumaquerodavid@gmail.com>
2024-06-30 15:21:18 +0200

    upgrade streamlit and add streamlit multi page navigation

--- {{cookiecutter.directory_name}}/src/{{cookiecutter.main_file}}.py
+++ {{cookiecutter.directory_name}}/src/{{cookiecutter.main_file}}.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.title("{{cookiecutter.project_name}}")
+
+st.set_page_config(page_title="{{cookiecutter.project_name}}", page_icon=":material/airline_seat_flat_angled:")
+
+page_1= st.Page("pages/page_1.py", title="Page 1", icon=":material/add_circle:")
+page_2 = st.Page("pages/page_2.py", title="Page 2", icon=":material/airline_seat_flat_angled:")
+
+
+pg = st.navigation([page_1, page_2])
+
+
+pg.run()
